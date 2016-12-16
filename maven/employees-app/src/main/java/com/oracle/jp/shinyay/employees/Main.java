1757ead240e6bd196bc25fd1d1abdf71b4d0c738
U-SYANAGIH-JP\syanagih <syanagih@SYANAGIH-JP.jp.oracle.com>
2016-12-16 19:42:23 +0900

    Modify for using on ACCS
    I add the procedure which read from Environment Variable

--- maven/employees-app/src/main/java/com/oracle/jp/shinyay/employees/Main.java
+++ maven/employees-app/src/main/java/com/oracle/jp/shinyay/employees/Main.java
@@ -9,13 +9,15 @@ import java.util.Optional;
  */
 public class Main {
 
-    public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
+    public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
+    public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
 
     public static void main(String[] args) throws Exception {
         String contextPath = "/";
         String appBase = ".";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.valueOf(port.orElse("8080")));
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080")));
+        tomcat.setHostname(HOSTNAME.orElse("localhost"));
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
